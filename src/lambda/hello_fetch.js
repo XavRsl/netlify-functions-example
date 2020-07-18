50e1e6ab873271f0d225f1ea5a3d7acf629b0586
Xavier Roussel <roussel.xavier@gmail.com>
2020-07-18 06:26:35 +0200

    Update hello_fetch.js

--- src/lambda/hello_fetch.js
+++ src/lambda/hello_fetch.js
@@ -5,7 +5,7 @@ const API_ENDPOINT = "https://doi.org/";
 exports.handler = async (event, context) => {
   const doi = event.queryStringParameters.doi;
   
-  return fetch(API_ENDPOINT, { headers: { "Accept": "application/vnd.citationstyles.csl+json;q=1.0" } })
+  return fetch(API_ENDPOINT + doi, { headers: { "Accept": "application/vnd.citationstyles.csl+json;q=1.0" } })
     .then(response => response.json())
     .then(data => ({
       statusCode: 200,
